674af89aebbd28e47258f8ad9cd960cb85e37673
JJair0 <166228362+JJair0@users.noreply.github.com>
2024-06-28 00:47:29 -0500

    Add files via upload

---
+++ Preguntas.py
@@ -0,0 +1,50 @@
+questions = [
+    {
+        "question": "¿Cuál es la capital de Francia?",
+        "options": ["a) Madrid", "b) París", "c) Roma", "d) Berlín"],
+        "answer": "b",
+        "hint": "Es conocida como la ciudad del amor."
+    },
+    {
+        "question": "¿Qué planeta es conocido como el Planeta Rojo?",
+        "options": ["a) Marte", "b) Júpiter", "c) Saturno", "d) Venus"],
+        "answer": "a",
+        "hint": "El nombre de este planeta está asociado con el dios romano de la guerra."
+    },
+    {
+        "question": "¿Quién escribió 'Don Quijote'?",
+        "options": ["a) Gabriel García Márquez", "b) Pablo Neruda", "c) Miguel de Cervantes", "d) Jorge Luis Borges"],
+        "answer": "c",
+        "hint": "Este autor es español."
+    },
+    {
+        "question": "¿Cuál es el río más largo del mundo?",
+        "options": ["a) Nilo", "b) Amazonas", "c) Yangtsé", "d) Misisipi"],
+        "answer": "b",
+        "hint": "Este río se encuentra en Sudamérica y es conocido por su vasta biodiversidad."
+    },
+    {
+        "question": "¿Cuál es el océano más grande del mundo?",
+        "options": ["a) Atlántico", "b) Índico", "c) Pacífico", "d) Ártico"],
+        "answer": "c",
+        "hint": "Este océano cubre más del 30% de la superficie de la Tierra."
+    },
+    {
+        "question": "¿En qué año llegó el hombre a la Luna?",
+        "options": ["a) 1965", "b) 1969", "c) 1972", "d) 1975"],
+        "answer": "b",
+        "hint": "Fue durante la misión del Apolo 11."
+    },
+    {
+        "question": "¿Cuál es el país más grande del mundo?",
+        "options": ["a) Canadá", "b) China", "c) Rusia", "d) Estados Unidos"],
+        "answer": "c",
+        "hint": "Este país se extiende por dos continentes, Europa y Asia."
+    },
+    {
+        "question": "¿Quién pintó la Mona Lisa?",
+        "options": ["a) Vincent van Gogh", "b) Pablo Picasso", "c) Leonardo da Vinci", "d) Michelangelo"],
+        "answer": "c",
+        "hint": "Es una de las obras más famosas del Renacimiento italiano."
+    }
+]
